a22ddc073c1cd3082a8b4b1e6a5f0d99836a6923
AkiraXDDD <oaksoeaung23@gmail.com>
2026-09-21 13:23:50 +0700

    Week 5 work first one

---
+++ Week_5/test_collection.py
@@ -0,0 +1,14 @@
+def test_list_equality():
+    assert [1,2,3] == [1,2,3]
+
+def test_list_content():
+    result = [3,2,1]
+    assert sorted(result) == [1,2,3]
+
+def test_dict_equality():
+    expected = {"Name": "Alice", "Age": 30}
+    actual = {"Age": 30, "Name": "Alice"}
+    assert actual == expected
+
+def test_set_operation():
+    assert {1,2,3} & {2,3,4} == {2,3}
